ef30450fb67f560c17309fb4a4b23a8a46decb65
Darshan <ldarsh04@gmail.com>
2023-09-20 23:44:14 -0400

    everything

---
+++ Pages/Freight_Forwarding.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Amity_Global.Pages
+{
+	public class Freight_ForwardingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/ISO_Tanks_and_Flexi_Tanks.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Amity_Global.Pages
+{
+	public class ISO_Tanks_and_Flexi_TanksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
